68008136707a631c819c1850b0ba5ec4cfce3f1d
Jamswhat2 <jamswhat2@Jamess-MacBook-Pro.local>
2023-03-02 12:42:08 -0500

    added comment @ UserPool [test]

--- frontend/src/UserPool.js
+++ frontend/src/UserPool.js
@@ -1,5 +1,7 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
+
+// need to pull UserPoolID, ClientId from the database;
 const poolData = {
     UserPoolId: "us-east-2_Q9XVyFwCY",
     ClientId: "3ejhi1tc4m7tkm0uk8tf8e5jq"
