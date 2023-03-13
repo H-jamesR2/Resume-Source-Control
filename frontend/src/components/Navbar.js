c4a28e027591f80d098892f7fb7613b85ae7ad08
qhe1332 <cherryhe5@gmail.com>
2023-03-13 15:32:24 -0400

    edit nav

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link, NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import backButton from "../images/left-arrow.png"
 import '../cssFiles/navbar.css';
@@ -8,26 +9,27 @@ import '../cssFiles/navbar.css';
 function NavBar(prop) {
     const [isOpen, setIsOpen] = useState(false);
 
-    function OpenSideBar()
-    {
+    function OpenSideBar() {
         setIsOpen(true);
     }
-    function CloseSideBar()
-    {
+    function CloseSideBar() {
         setIsOpen(false);
     }
 
     return (
-        <div id="nav-wrapper"  className={isOpen ? "open" : ""}>
-            <div className="sidebar-top">
-                <img src={backButton} onClick={OpenSideBar} className="icon"/>
-                <div>Menu</div>
+        <div class="nav-wrapper">
+            <FontAwesomeIcon icon="fa-solid fa-chevron-left" />
+            <div id="sidebar-wrapper" className={isOpen ? "open" : ""}>
+                <div className="sidebar-top">
+                    <img src={backButton} onClick={OpenSideBar} className="icon" />
+                    <div>Menu</div>
+                </div>
+                <nav className="sidebar-bottom">
+                    <li><Link to="/resume">Resumes</Link></li>
+                    <li><Link to="/blocks">Blocks</Link></li>
+                    <li><Link to="/applications">Applications</Link></li>
+                </nav>
             </div>
-        	<nav className="sidebar">
-        		<li><Link to="/resume">Resumes</Link></li>
-                <li><Link to="/blocks">Blocks</Link></li>
-                <li><Link to="/applications">Applications</Link></li>
-        	</nav>
         </div>
     );
 }
