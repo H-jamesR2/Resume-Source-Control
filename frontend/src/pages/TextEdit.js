58a6d54025ab06f0e1f52aeaacbd35703cb95fd8
Jamswhat2 <jamswhat2@Jamess-MBP.fios-router.home>
2023-03-12 22:32:52 -0400

    readded TextEdit js+css onto text-edit-system branch with main files

---
+++ frontend/src/pages/TextEdit.js
@@ -0,0 +1,23 @@
+// imports
+
+
+// Consider using markdown or lexical(facebook) (as base editor)
+
+// Formatting 
+    // Dates on Resume:
+    //  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ---> adds 6 spaces between characters
+    // Indenting bullet points --> need to find a way to configure
+    //  spacing of bullet points.
+    // --> need to figure out a way to map "spacing" to TAB key on browser;
+    // --> need to figure out a wayt o map "indentation" on highlighted line:
+        // to command/ctrl + "[" or "]" keys
+        // - consider having header bar to configure indentation settings.
+
+// const TextEdit()
+
+// Tool Bar
+// Text/Block-Space
+
+// export default TextEdit
+
+// npm install slate slate-react
