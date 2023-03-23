8f7fb75e153315bb1cd6f4e27fcea50213ef7dd1
WilliamM24 <112582885+WilliamM24@users.noreply.github.com>
2023-03-23 13:44:25 -0400

    BlockEditor Site

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,181 +1,32 @@
-// Import React dependencies.
-import React, { useCallback, useState } from 'react'
-// Import the Slate editor factory.
-import { createEditor } from 'slate'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Outlet, Link, NavLink } from "react-router-dom";
+import './App.css';
+import Homepage from './pages/Homepage';
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import './cssFiles/Login.css';
+import Mainpage from "./pages/Mainpage";
+import Resume from "./pages/Resume";
+import Blocks from "./pages/Blocks";
+import Settings from "./pages/Settings";
+import Application from "./pages/Application";
+import BlockEditor from "./pages/BlockEditor";
 
-// Import the Slate components and React plugin.
-import { Slate, Editable, withReact } from 'slate-react'
-import { Editor, Transforms, Text } from 'slate'
-
-// Define our own custom set of helpers.
-const CustomEditor = {
-  isBoldMarkActive(editor) {
-    const [match] = Editor.nodes(editor, {
-      match: n => n.bold === true,
-      universal: true,
-    })
-
-    return !!match
-  },
-
-  isItalicMarkActive(editor) {
-    const [match] = Editor.nodes(editor, {
-      match: n => n.italic === true,
-      universal: true,
-    })
-
-    return !!match
-  },
-
-  isCodeBlockActive(editor) {
-    const [match] = Editor.nodes(editor, {
-      match: n => n.type === 'code',
-    })
-
-    return !!match
-  },
-
-  toggleBoldMark(editor) {
-    const isActive = CustomEditor.isBoldMarkActive(editor)
-    Transforms.setNodes(
-      editor,
-      { bold: isActive ? null : true },
-      { match: n => Text.isText(n), split: true }
-    )
-  },
-
-  toggleItalicMark(editor) {
-    const isActive = CustomEditor.isItalicMarkActive(editor)
-    Transforms.setNodes(
-      editor,
-      { italic: isActive ? null : true },
-      { match: n => Text.isText(n), split: true }
-    )
-  },
-
-  toggleCodeBlock(editor) {
-    const isActive = CustomEditor.isCodeBlockActive(editor)
-    Transforms.setNodes(
-      editor,
-      { type: isActive ? null : 'code' },
-      { match: n => Editor.isBlock(editor, n) }
-    )
-  },
-}
-
-// Add the initial value.
-const initialValue = [
-  {
-    type: 'paragraph',
-    children: [{ text: 'I have currently made it so the text can be bolded or italicized.' }],
-  },
-]
-
-const App = () => {
-  const [editor] = useState(() => withReact(createEditor()))
-
-  const renderElement = useCallback(props => {
-    switch (props.element.type) {
-      case 'code':
-        return <CodeElement {...props} />
-      default:
-        return <DefaultElement {...props} />
-    }
-  }, [])
-
-  const renderLeaf = useCallback(props => {
-    return <Leaf {...props} />
-  }, [])
-
+export default function App() {
   return (
-    // Add a toolbar with buttons that call the same methods.
-    <Slate editor={editor} value={initialValue}>
       <div>
-        <button
-          onMouseDown={event => {
-            event.preventDefault()
-            CustomEditor.toggleBoldMark(editor)
-          }}
-        >
-          Bold
-        </button>
-
-        <button
-          onMouseDown={event => {
-            event.preventDefault()
-            CustomEditor.toggleItalicMark(editor)
-          }}
-        >
-          Italic
-        </button>
-
-        {/* <button
-          onMouseDown={event => {
-            event.preventDefault()
-            CustomEditor.toggleCodeBlock(editor)
-          }}
-        >
-          Code Block
-        </button> */}
+        <Routes>                  
+            <Route index element = {<Homepage/>} />
+            <Route path="/signup" element={<Signup/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/mainpage" element={<Mainpage/>}/>
+            <Route path="/resume" element={<Resume/>}/>
+            <Route path="/blocks" element={<Blocks/>}/>            
+            <Route path="/settings" element={<Settings/>}/>            
+            <Route path="/applications" element={<Application/>}/>
+            <Route path="/blockeditor" element={<BlockEditor/>}/>
+        </Routes>
       </div>
-      <Editable
-        editor={editor}
-        renderElement={renderElement}
-        renderLeaf={renderLeaf}
-        onKeyDown={event => {
-          if (!event.ctrlKey) {
-            return
-          }
-
-          switch (event.key) {
-            case '`': {
-              event.preventDefault()
-              CustomEditor.toggleCodeBlock(editor)
-              break
-            }
-
-            case 'b': {
-              event.preventDefault()
-              CustomEditor.toggleBoldMark(editor)
-              break
-            }
-
-            case 'i': {
-              event.preventDefault()
-              CustomEditor.toggleItalicMark(editor)
-              break
-            }
-          }
-        }}
-      />
-    </Slate>
-  )
-}
-const Leaf = props => {
-  return (
-    <span
-      {...props.attributes}
-      style={{ 
-        fontWeight: props.leaf.bold ? 'bold' : 'normal',
-        fontStyle: props.leaf.italic ? 'italic' : 'normal', 
-      }}
-    >
-      {props.children}
-    </span>
-  )
+  );
 }
 
-const CodeElement = props => {
-  return (
-    <pre {...props.attributes}>
-      <code>{props.children}</code>
-    </pre>
-  )
-}
-
-const DefaultElement = props => {
-  return <p {...props.attributes}>{props.children}</p>
-}
-
-
-export default App;
