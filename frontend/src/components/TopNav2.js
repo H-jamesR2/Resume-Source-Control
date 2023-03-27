8b05e923c231e7faddcd42f67cb8765714c12346
A278PlusPi <anthonyjohn.regner61@myhunter.cuny.edu>
2023-03-26 21:38:07 -0400

    Simplified top navigation bar!

--- frontend/src/components/TopNav2.js
+++ frontend/src/components/TopNav2.js
@@ -8,60 +8,45 @@ import Person1 from '../images/Person-1.png'
 
 const Username = "Anthony";
 
-class TopNav2 extends Component {
-constructor() {
-    super();
-    this.state = {
-      name: "React",
-      add_items: false,
-      user_settings: false
-    };
-    this.hideComponent = this.hideComponent.bind(this);
-  }
 
-  hideComponent(varname) {
-    console.log(varname);
-    switch (varname) {
-      case "add_items":
-        this.setState({ add_items: !this.state.add_items });
-        break;
-      case "user_settings":
-        this.setState({ user_settings: !this.state.user_settings });
-        break;
-      default: return;
-    }
-  }
+function TopNav2(props) {
 
-render() {
-    const {add_items, user_settings} = this.state;
     return (
         // Resume Logo [Icon] should go to About-page [Extra]
         // Once logged in, User should have different nav-bar such that it is able to click to a:
         // User Profile [Icon]
         <div>
-            {add_items && <AddItems/>}
-            {user_settings && <UserMenu />}
             <div className="nav-bar">
                 <ul className="nav-bar-left">
                     <li><img className={"resume-logo"} src={logo}></img></li>
                     <li className='nav-item'><Link className='no-decor' to="/">Home</Link></li>                    
-                    <li onMouseOver={() => this.hideComponent("add_items")} className='nav-item'><b>+ ADD</b></li>
+                    <li className='nav-item'>
+                        <b>+ ADD</b>
+                    <div id="add-items" class="popup-menu">
+                        <div>
+                        <li className="popup-menu-item">Upload Resume</li>
+                        <li className="popup-menu-item">Create Resume</li>
+                        </div>
+                    </div>
+                    </li>
                 </ul>
                 <ul className="nav-bar-right">
                     <li 
-                        onMouseOver={() => this.hideComponent("user_settings")} 
-                        className='nav-item'
-                        style={{position: "relative", top: "-7px"}}>
+                        className='nav-item-right'>
                         <img id={"profile-picture-w"} src={Person1} ></img> Hi, {Username}
+                    <div id="user-menu" class="popup-menu">
+                        <div>
+                            <li className="popup-menu-item"><Link className='no-decor' to="/profile">Profile</Link></li>
+                            <li className="popup-menu-item"><Link className='no-decor' to="/settings">Account Settings</Link></li>
+                            <li className="popup-menu-item"><Link className='no-decor' to="/">Log Out</Link></li>
+                        </div>
+                    </div>
                     </li>
-                    {/* <li className='nav-item'><Link className='no-decor' to="/">Sign Out</Link></li> */}
                 </ul>
             </div>
             <Outlet />
         </div>
     );
-}
-
 }
 
 export default TopNav2;
