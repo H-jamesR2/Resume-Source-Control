2dc9ec3b39f0d351175f60155c8e1223968a26c1
Antonio <idiezag@gmail.com>
2023-03-28 14:42:33 -0400

    Added library and renamed two elements LogIn and SignUp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Outlet, Link, NavLink } from "react-router-dom";
 import './App.css';
 import Homepage from './pages/Homepage';
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
+import {Session} from "./components/UserContext"
+import SignUp from "./pages/Signup";
+import LogIn from "./pages/Login";
 import './cssFiles/Login.css';
 import Mainpage from "./pages/Mainpage";
 import Resume from "./pages/Resume";
@@ -15,10 +16,11 @@ import BlockEditor from "./pages/BlockEditor";
 export default function App() {
   return (
       <div>
+        <Session>
         <Routes>                  
             <Route index element = {<Homepage/>} />
-            <Route path="/signup" element={<Signup/>}/>
-            <Route path="/login" element={<Login/>}/>
+            <Route path="/signup" element={<SignUp/>}/>
+            <Route path="/login" element={<LogIn/>}/>
             <Route path="/mainpage" element={<Mainpage/>}/>
             <Route path="/resume" element={<Resume/>}/>
             <Route path="/blocks" element={<Blocks/>}/>            
@@ -26,6 +28,7 @@ export default function App() {
             <Route path="/applications" element={<Application/>}/>
             <Route path="/blockeditor" element={<BlockEditor/>}/>
         </Routes>
+        </Session>
       </div>
   );
 }
