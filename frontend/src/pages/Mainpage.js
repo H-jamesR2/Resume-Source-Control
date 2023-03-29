d1169f70f54671ec34cef7d062f64ff37679edf7
Antonio <idiezag@gmail.com>
2023-03-29 09:30:07 -0400

    Added SessionContext

--- frontend/src/pages/Mainpage.js
+++ frontend/src/pages/Mainpage.js
@@ -1,24 +1,36 @@
-import React from "react";
+import React, {useContext, useState, useEffect} from "react";
 import { BrowserRouter as Router, Routes, Route, Link, NavLink } from "react-router-dom";
 import TopNav2 from "../components/TopNav2";
 import NavBar from "../components/Navbar";
 import Settings from "./Settings";
+import { SessionContext } from "../components/UserContext";
+
 
 
 function Mainpage(prop)
 {
+    const {getUserSession} = useContext(SessionContext)
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    useEffect(()=>{
+        getUserSession().then(()=>{setIsLoggedIn(true)})
+    },[])
+    
     return(    
         <div>
-            <TopNav2/>
-            <div className="page-wrapper">
-                <NavBar/>
-                <div className="main-content">
-                    {/* add page content here */}
-                    Main page after login
-                    <br></br>                
-                    <Link to="/settings">Account Settings</Link>
+            {isLoggedIn && (
+                <>   
+                <TopNav2/>
+                <div className="page-wrapper">
+                    <NavBar/>
+                    <div className="main-content">
+                        {/* add page content here */}
+                        Main page after login
+                        <br></br>                
+                        <Link to="/settings">Account Settings</Link>
+                    </div>
                 </div>
-            </div>
+                </>
+            )}
         </div>
     );
 }
