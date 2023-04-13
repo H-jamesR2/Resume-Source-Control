43c581095b6f703e864490751fdae7118a0da918
A278PlusPi <anthonyjohn.regner61@myhunter.cuny.edu>
2023-04-13 11:15:46 -0400

    Finished (mostly) with the source control sidebar... Credits: W3Schools custom radio buttons

--- frontend/src/components/SourceControl.js
+++ frontend/src/components/SourceControl.js
@@ -15,6 +15,15 @@ function SourceControl(prop) {
         setIsOpen(false);
     }
 
+function SelectBranch(props) {
+  return (
+    <label className="version-control-container" for={props.name}>{props.display}                  
+      <input type="radio" className="sidebar-sc-branch" id={props.name} checked={props.default} name="branch" value="{props.value}"></input>
+      <span className="custom-radio"></span><br/>
+    </label>
+  )
+}
+
     return (
         <div className="nav-wrapper">
             <div id="sidebar-sc-wrapper" className={isOpen ? "open" : "close"}>
@@ -26,13 +35,28 @@ function SourceControl(prop) {
                     <FontAwesomeIcon icon={faAngleRight} size="lg" className="icon" onClick={CloseSideBar} style={{marginLeft:"15px"}}/>
                 </div>
                 <nav className="sidebar-sc-bottom">
-                    <li><div className="sidebar-sc-text">Branch 1</div></li>
-                      <li><div className="sidebar-sc-text" style={{fontSize:"70%",marginLeft:"12px"}}>Sub-Branch 1</div></li>
-                      <li><div className="sidebar-sc-text" style={{fontSize:"70%",marginLeft:"12px"}}>Sub-Branch 2</div></li>
+                    <li><div className="sidebar-sc-text">Software Engineering</div></li>
+                    <div className="sidebar-sc-text" style={{marginBottom:"15px"}}>
+                      <SelectBranch name="branch-1-s1" value="branch-1-sub-branch-1" display="A software engineer takes it place"/>
+                      <SelectBranch name="branch-1-s2" value="branch-1-sub-branch-2" default="checked" display="Another one, but this time, it more of frontend"/>
+                    </div>
                     <li><div className="sidebar-sc-text">Branch 2</div></li>
+                    <div className="sidebar-sc-text" style={{marginBottom:"15px"}}>
+                      <SelectBranch name="branch-2-s1" value="branch-2-sub-branch-1" display="Sub-Branch 1"/>
+                      <SelectBranch name="branch-2-s2" value="branch-2-sub-branch-2" display="Sub-Branch 2"/>
+                      <SelectBranch name="branch-2-s3" value="branch-2-sub-branch-3" display="Sub-Branch 3"/>
+                      <SelectBranch name="branch-2-s4" value="branch-2-sub-branch-4" display="Sub-Branch 4"/>
+                    </div>
                     <li><div className="sidebar-sc-text">Branch 3</div></li>
-                      <li><div className="sidebar-sc-text" style={{fontSize:"70%",marginLeft:"12px"}}>Sub-Branch 1</div></li>
+                    <div className="sidebar-sc-text" style={{marginBottom:"15px"}}>
+                      <SelectBranch name="branch-3-s1" value="branch-3-sub-branch-1" display="Sub-Branch 1"/>
+                    </div>
                 </nav>
+
+                <div className="sidebar-sc-text">
+                  <button className="small">Use this version</button>
+                  <button className="small">Create new branch</button>
+                </div>
             </div>
         </div>
     );
