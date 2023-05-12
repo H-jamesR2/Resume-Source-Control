ff3c3a9b6fca43ccb6621a0f0c5e152c955a1820
WilliamM24 <112582885+WilliamM24@users.noreply.github.com>
2023-05-12 12:20:58 -0400

    connect to AWS RDS server

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,24 @@
-const {Pool} = require('pg') //destructuring Pool object
+// const {Pool} = require('pg') //destructuring Pool object
+const Pool = require('pg').Pool
 
-const pool  = new Pool();
+const pool  = new Pool(
+    {
+    user: 'postgres',
+    host: 'resumeapp.cwnjtcnkyave.us-east-2.rds.amazonaws.com',
+    database: 'postgres',
+    password: 'PasswordDB',
+}
+);
+
+pool.connect(function(err)
+{
+    if(err){
+        console.error('database connection failed:' + err.stack);
+        return;
+    }
+    console.log('connected to database');
+});
+
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
