3c2f926d760d443978f050f06625e6ab61229884
WilliamM24 <wmak62@gmail.com>
2023-05-15 08:09:21 -0400

    Connected to Database

--- frontend/src/pages/Application.js
+++ frontend/src/pages/Application.js
@@ -6,6 +6,12 @@ import TopNav from "../components/TopNav";
 import AppTable from "../components/AppTable";
 import { Navigate } from "react-router-dom";
 import { SessionContext } from "../components/UserContext";
+import ResumeUrl from '../api/ResumeUrl';
+import { UniContext } from "../context/UniContext";
+import { Button, Table, Tag, Form, Modal, Input, Select, DatePicker, ConfigProvider, theme } from "antd";
+import { FileTextTwoTone, CloseCircleTwoTone, ScheduleOutlined, EditTwoTone, DeleteTwoTone, PlusOutlined, ClockCircleOutlined, CheckCircleOutlined, CarryOutOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
+
 
 function Application(prop) {
 
@@ -21,6 +27,152 @@ function Application(prop) {
 
     }, []);
 
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [editingRecord, setEditingRecord] = useState(null);
+    const [position, setPosition] = useState("")
+    const [company, setCompany] = useState("")
+    const [submission_date, SetSubmissionDate] = useState("")
+    const [status, setStatus] = useState("")
+    const [resume, SetResume] = useState("")
+    const [applications,setApplications] = useState([]);
+    let navigate = useNavigate();
+
+
+    // const {deleteApplications, setDeleteApplications} = useContext(UniContext)
+
+
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        try{
+            const response = await ResumeUrl.post("v1/applications", {
+                position,
+                company,
+                submission_date,
+                status,
+                resume,
+        })
+        addApplicatons(response.data.data.application)
+        }catch(err){}
+        window.location.reload(false);
+    }
+
+    const handleDelete =  async (e, id) =>{
+        try{
+            const response = await ResumeUrl.delete( `/v1/applications/${id}`)
+            setApplications(applications.filter(application=>{
+                return application.id !== id
+            }))
+            console.log(response)
+        }catch (err) {
+    
+        }
+    }
+    const editApplicationRecord = (record) => {
+        setIsEditing(true);
+        setEditingRecord({ ...record });
+    };    
+    const handleEdit = (e, id) => {
+        navigate(`/data/${id}/updateApplication`);
+      }
+
+    useEffect (() =>{
+        const fetchData = async () =>{
+            try{
+            const response = await ResumeUrl.get('/v1/applications');
+            setApplications(response.data.data.applications)
+            console.log('Application: ', response)
+        }catch(err){}}
+        fetchData();
+    },[])
+
+
+    const handleButtonClick = () => {
+        setVisible(true);
+      };
+
+
+    const columns = [
+        {
+            title: "id",
+            dataIndex: "id",
+            key: "id",
+            hidden: true
+        },
+        {
+            title: "Position",
+            dataIndex: "position",
+            key: "position"
+        },
+        {
+            title: "Company",
+            dataIndex: "company",
+            key: "company"
+        },
+        {
+            title: "Submission Date",
+            dataIndex: "submission_date",
+            key: "submission_date"
+        },
+        {
+            title: "Status",
+            dataIndex: "status",
+            key: "status",
+            render: (_, record) => {
+                let tagColor;
+                let icon;
+                if (record.status === "Applied") {
+                    tagColor = "default";
+                    icon = <ClockCircleOutlined color="gray" />;
+                }
+                if (record.status === "Interviewing") {
+                    tagColor = "cyan";
+                    icon = <ScheduleOutlined color="cyan" />;
+                }
+                if (record.status === "Under Consideration") {
+                    tagColor = "gold";
+                    icon = <CarryOutOutlined color="gray" />;
+                }
+                if (record.status === "Hired") {
+                    tagColor = "green";
+                    icon = <CheckCircleOutlined color="green" />;
+                }
+                if (record.status === "No Offer") {
+                    tagColor = "red";
+                    icon = <CloseCircleTwoTone twoToneColor="red" />;
+                }
+                return <>
+                    <Tag className="tag-text" color={tagColor} icon={icon} key={record.status} >
+                        <span>{record.status}</span>
+                    </Tag>
+                </>
+            }
+        },
+        {
+            title: "Resume Used",
+            key: "resume_id",
+            dataIndex: "resume_id",
+            render: (_, record) => {
+                return <>
+                    <Tag className="tag-text" color="geekblue" key={record.resume} >
+                        <FileTextTwoTone />
+                        <Link to="/" className="resume-tag-text">{record.resume}</Link>
+                    </Tag>
+                </>
+            }
+        },
+        {
+            title: "Action",
+            render: (_, record) => {
+                return <>
+                    <EditTwoTone onClick={() => { editApplicationRecord(record) }} />
+                    <DeleteTwoTone onClick={(e) => handleDelete(e, application.id)} className="action-icon" twoToneColor="red" />
+                </>
+            }
+        },
+
+    ].filter(item => !item.hidden);
     const dataSource = [
         {
             id: 1,
@@ -56,12 +208,102 @@ function Application(prop) {
                         <NavBar />
                         <div className="main-content">
                             <div>Applications Tracker</div>
+                            
+                            <Table className="table-container" dataSource={applications} columns = {columns}/>
+                            <table>
+                                <thead>
+                                <tr>
+                                    <th>Position</th>
+                                    <th>Company</th>
+                                    <th>Submission Date</th>
+                                    <th>Status</th>
+                                    <th>Resume</th>
+                                    <th>Edit</th>
+                                    <th>Delete</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {applications && 
+                                            applications.map(application =>{
+                                            return(
+                                                <tr key={application.id}>
+                                                <td>{application.position}</td>
+                                                <td>{application.company}</td>
+                                                <td>{application.submission_date}</td>
+                                                <td>{application.status}</td>
+                                                <td>{application.resume}</td>
+                                                <td><button onClick={(e)=> handleEdit(e, application.id)}>Edit</button></td>
+                                                <td><button onClick={(e) => handleDelete(e, application.id)} >Delete</button></td>
+                                            </tr>
+
+                                            )
+                                            
+                                        } )}
+                                
+                                </tbody>
+                            </table>
+                            {/* <ApplicationList/> */}
+                            <ConfigProvider
+                                theme={{
+                                    token: {
+                                        colorPrimary: "#6892FE",
+                                        fontFamily: "Lekton",
+                                        fontSize: "18px",
+                                    },
+                                }}>
+                                    <br/>
+                                    <Form>
+                                    <Form.Item name='position' label='Position' style={{ width: 250 }} size='large'
+                            rules={[
+                                { required: true, message: "Please input the title of job position!" }
+                            ]}
+                        >
+                            <Input value={position} onChange={e=>setPosition(e.target.value)} type='text' />
+                        </Form.Item>
+                        <Form.Item name='company' label='Company' style={{ width: 250 }} size='large'
+                            rules={[
+                                { required: true, message: "Please input the company name!" }
+                            ]}
+                        >
+                            <Input value={company} onChange={e=>setCompany(e.target.value)} type='text' />
+                        </Form.Item>
+                        <Form.Item name='submissionDate' label='Date Applied' size='large'
+                            rules={[
+                                { required: true, message: "Please input the date applied!" }
+                            ]}
+                        >
+                            <input value={submission_date} onChange={e=>SetSubmissionDate(e.target.value)} type='date' />
+                        </Form.Item>
+                        <Form.Item name='status ' label='Status: (Applied, Interviewing, Under Considerations, Hired, No Offer)' style={{ width: 1000 }} size='large'
+                            rules={[
+                                { required: true, message: "Please select your application status! " }
+                            ]}
+                        >
+                            <Input value={status} onChange={e=>setStatus(e.target.value)} type='text' />
+                        </Form.Item>
+
+                        <Form.Item name='resume' label='Resume Used' style={{ width: 250 }} size='large'
+                            rules={[
+                                { required: true, message: "Required!" }
+                            ]}
+                        >
+                            <Select value={resume} onChange={e=>SetResume(e.target.value)}>
+                                <Option value='jake ryan'>Jake Ryan</Option>
+                                 </Select>
+                    
+                        </Form.Item>
+                        <button onClick={handleSubmit} type='submit'>Add</button>
+
+                        
+                                    </Form>
+                            </ConfigProvider>
                             <AppTable data={dataSource} />
+
                         </div>
                     </div>
                 </>
             }
-            { !isLoggedIn && (
+        { !isLoggedIn && (
             <div>   
                 <TopNav/>
                 <div className="page-wrapper">
@@ -76,3 +318,6 @@ function Application(prop) {
 }
 
 export default Application;
+
+
+
