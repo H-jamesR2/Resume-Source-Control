42afdfecaf3728d4ea3eebc24c491894c5a73e34
TheStress <lieu.nelson386@gmail.com>
2023-05-15 11:41:14 -0400

    Update setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:6000',
+      target: 'http://localhost:5000',
       changeOrigin: true,
     })
   );
